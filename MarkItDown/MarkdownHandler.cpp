848b98a7a6161490456d2b5fbffc236cd5ae4751
hbouchet <hbouchet@student.42.fr>
2020-11-25 16:45:26 +0100

    feat: add prependEachLine method

--- MarkItDown/MarkdownHandler.cpp
+++ MarkItDown/MarkdownHandler.cpp
@@ -19,6 +19,25 @@ MarkdownHandler::MarkdownHandler()
 //    cursor.insertText(str);
 //}
 
+bool    MarkdownHandler::isAlreadyPrefixed(QTextEdit *editor, QRegularExpression rx)
+{
+    QTextCursor     cursor = editor->textCursor();
+    QTextDocument   *doc = editor->document();
+
+    int start = doc->findBlock(cursor.selectionStart()).blockNumber();
+    int end = doc->findBlock(cursor.selectionEnd()).blockNumber();
+    cursor.setPosition(cursor.selectionStart());
+
+    for (int i = start; i <= end; i++)
+    {
+        cursor.movePosition(QTextCursor::StartOfBlock);
+        if (cursor.block().text().contains(rx))
+            return true;
+        cursor.movePosition(QTextCursor::NextBlock);
+    }
+    return false;
+}
+
 void    MarkdownHandler::prependEachLine(QTextEdit *editor, QString str)
 {
     QTextCursor cursor = editor->textCursor();
@@ -30,8 +49,32 @@ void    MarkdownHandler::prependEachLine(QTextEdit *editor, QString str)
 
     for (int i = start; i <= end; i++)
     {
+        if (cursor.block().begin() == cursor.block().end())
+            continue;
         cursor.movePosition(QTextCursor::StartOfBlock);
         cursor.insertText(str);
+        cursor.movePosition(QTextCursor::NextBlock);
+    }
+}
+
+void    MarkdownHandler::unPrependEachLine(QTextEdit *editor, QRegularExpression rx)
+{
+    QTextCursor     cursor = editor->textCursor();
+    QTextDocument   *doc = editor->document();
+
+    int start = doc->findBlock(cursor.selectionStart()).blockNumber();
+    int end = doc->findBlock(cursor.selectionEnd()).blockNumber();
+    cursor.setPosition(cursor.selectionStart());
+
+    for (int i = start; i <= end; i++)
+    {
+        cursor.movePosition(QTextCursor::StartOfBlock);
+        if (cursor.block().text().contains(rx))
+        {
+//            cursor.block().text().remove(rx); //doesn't work
+            cursor.movePosition(QTextCursor::NextWord, QTextCursor::KeepAnchor);
+            cursor.insertText(cursor.selectedText().remove(rx));
+        }
         cursor.movePosition(QTextCursor::NextBlock);
     }
 }
@@ -60,7 +103,7 @@ void    MarkdownHandler::wrapText(QTextEdit *editor, QString leftWrap, QString r
 void    MarkdownHandler::wrapParagraph(QTextEdit *editor, QString leftWrap, QString rightWrap)
 {
     QTextCursor cursor = editor->textCursor();
-    QString     tmp = cursor.selectedText();
+//    QString     tmp = cursor.selectedText();
     int         end = cursor.selectionEnd();
 
     if (rightWrap == NULL)
--- MarkItDown/MarkdownHandler.h
+++ MarkItDown/MarkdownHandler.h
@@ -5,18 +5,21 @@
 #include <QTextEdit>
 #include <QTextCursor>
 #include <QTextBlock>
+#include <QRegularExpression>
 
 class MarkdownHandler
 {
 public:
     MarkdownHandler();
 
-    static void wrapLine(QString leftWrap, QString rightWrap = NULL);
+//    static void wrapLine(QString leftWrap, QString rightWrap = NULL);
+    static bool isAlreadyPrefixed(QTextEdit *editor, QRegularExpression rx);
     static void wrapText(QTextEdit *editor, QString leftWrap, QString rightWrap = NULL);
     static void wrapParagraph(QTextEdit *editor, QString leftWrap, QString rightWrap = NULL);
-    static void prependLine(QTextEdit *editor, QString str);
-    static void appendLine(QTextEdit *editor, QString str);
+//    static void prependLine(QTextEdit *editor, QString str);
+//    static void appendLine(QTextEdit *editor, QString str);
     static void prependEachLine(QTextEdit *editor, QString str);
+    static void unPrependEachLine(QTextEdit *editor, QRegularExpression rx);
 };
 
 #endif // MARKDOWNHANDLER_H
